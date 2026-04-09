3ca2147e53e15f98622acb8635e67eadec4e22ce
Sergei Levashov <cephei.l@yandex.ru>
2026-04-10 02:00:18 +0300

    feat: Simple Function Calling

--- src/main/kotlin/ru/cephei/Main.kt
+++ src/main/kotlin/ru/cephei/Main.kt
@@ -4,18 +4,23 @@ import kotlinx.coroutines.runBlocking
 import ru.cephei.agents.AIAgent
 import ru.cephei.agents.DefaultPromptExecutor
 import ru.cephei.services.MockLLMService
+import ru.cephei.tools.*
 
 fun main() = runBlocking {
 
     val llmService = MockLLMService()
     val executor = DefaultPromptExecutor(llmService)
 
+    val tools = ToolRegistry(listOf(EchoTool(), TimeTool()))
+
     val agent = AIAgent(
-        name = "ProdAgent",
+        name = "AgentWithTools",
         executor = executor,
-        systemPrompt = "You are a helpful assistant"
+        systemPrompt = "You are a helpful assistant",
+        toolRegistry = tools
     )
 
-    val result = agent.run("Hello!")
-    println(result)
+    println(agent.run("Hello"))
+    println(agent.run("tool:echo"))
+    println(agent.run("tool:time"))
 }
